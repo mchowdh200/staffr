2db30e920f5be972bbe42f2047a60595160b38ff
Murad <mchowdh200@gmail.com>
2024-10-07 16:26:21 -0600

    fix jax hw model

--- jax_model_hw.py
+++ jax_model_hw.py
@@ -1,15 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from functools import partial
 
 import jax
 import jax.numpy as jnp
-from jax import lax
+from flax import struct
+from jax import lax, tree_util
 from jax.scipy.stats import norm
 from jax.typing import ArrayLike
 from ott.tools.k_means import k_means
 
+# jax.config.update("jax_debug_nans", True)
 
-@dataclass(kw_only=True)
+@struct.dataclass
 class State:
     pi: jax.Array
     mu: jax.Array
@@ -21,26 +23,26 @@ class State:
 
 
 @partial(jax.jit, static_argnums=(1, 2, 3))
-def fit_hw_model(X: jax.Array, n: int, c: float = 1e-4, max_iter: int = 10_000):
-    f = X.reshape(-1, 1)
-    km_out = k_means(f, 3)
-    centers = jnp.sort(km_out.centroids.ravel())
+def fit_hw_model(X: jax.Array, n: int, c: float = 1e-5, max_iter: int = 10_000):
+    km_out = k_means(X.reshape((-1, 1)), 3)
+    centers = jnp.sort(km_out.centroids.ravel().sort()[1:])
+
     initial_state = State(
-        pi=jnp.ones(3) / 3,
-        mu=centers[1:],
+        pi=jnp.ones(3, dtype=jnp.float32) / 3.,
+        mu=centers,
         sigma=jnp.ones(2) * jnp.std(X),
         r=jnp.zeros((3, n)),
         iterations=0,
         log_likelihood=0.0,
-        prev_log_likelihood=jnp.inf,
+        prev_log_likelihood=1.0,
     )
 
     ## ------------------------------------------------------------------------
     ## EM algorithm loop
     ## ------------------------------------------------------------------------
-    def while_loop(initial_state: State):
+    def EM_main_loop(initial_state: State):
         def loop_condition(state: State):
-            return (state.iterations < max_iter) or (
+            return (state.iterations < max_iter) & (
                 jnp.abs(state.log_likelihood - state.prev_log_likelihood) > c
             )
 
@@ -56,70 +58,68 @@ def fit_hw_model(X: jax.Array, n: int, c: float = 1e-4, max_iter: int = 10_000):
                 state.prev_log_likelihood,
             )
 
+
             ## E-step -------------------------------------------------------------
             # zero component
-            r.at[0, X == 0].set(pi[0])
-            r.at[0, X != 0].set(0.0)
+            r = r.at[0].set(jnp.where(X == 0, pi[0], 0.0))
 
             # gaussian components
-            r.at[1, :].set(pi[1] * norm.pdf(X, mu[0], sigma[0]))
-            r.at[2, :].set(pi[2] * norm.pdf(X, mu[1], sigma[1]))
-            r = r / jnp.sum(r, axis=0)
+            r = r.at[1].set(pi[1] * norm.pdf(X, mu[0], sigma[0]))
+            r = r.at[2].set(pi[2] * norm.pdf(X, mu[1], sigma[1]))
+            r = r / jnp.maximum(jnp.sum(r, axis=0), 1e-6)
 
             ## M-step -------------------------------------------------------------
             # total responsibility of a component divided by total # of observations
-            pi = jnp.sum(r, axis=1) / n
+            pi = jnp.sum(r, axis=1) / jnp.float32(n)
 
             # MLE for gaussian means
-            mu.at[0].set(jnp.average(X, weights=r[1]))
-            mu.at[1].set(jnp.average(X, weights=r[2]))
+            mu = mu.at[0].set(jnp.average(X, weights=r[1]))
+            mu = mu.at[1].set(jnp.average(X, weights=r[2]))
 
             # constrain mu[1] = 2mu[0]
-            mu.at[0].set((mu[0] + 0.5 * mu[1]) / 2)
-            mu.at[1].set(2 * mu[0])
+            mu = mu.at[0].set((mu[0] + 0.5 * mu[1]) / 2)
+            mu = mu.at[1].set(2 * mu[0])
 
             # MLE for gaussian variances
-            sigma.at[0].set(jnp.sqrt(jnp.average((X - mu[0]) ** 2, weights=r[1])))
-            sigma.at[1].set(jnp.sqrt(jnp.average((X - mu[1]) ** 2, weights=r[2])))
+            sigma = sigma.at[0].set(jnp.sqrt(jnp.average((X - mu[0]) ** 2, weights=r[1])))
+            sigma = sigma.at[1].set(jnp.sqrt(jnp.average((X - mu[1]) ** 2, weights=r[2])))
             sigma = jnp.maximum(sigma, 1e-6)
 
             ## Compute log likelihood ---------------------------------------------
             hurdle = jnp.where(X == 0, pi[0], 0)
             gmm = pi[1] * norm.pdf(X, mu[0], sigma[0]) + pi[2] * norm.pdf(
                 X, mu[1], sigma[1]
-            )
-            prev_log_likelihood = log_likelihood
-            log_likelihood = jnp.sum(jnp.log(hurdle + gmm))
-            iterations = iterations + 1
+            ) + 1e-6
 
             return State(
                 pi=pi,
                 mu=mu,
                 sigma=sigma,
                 r=r,
-                iterations=iterations,
-                log_likelihood=log_likelihood,
-                prev_log_likelihood=prev_log_likelihood,
+                iterations=iterations + 1,
+                log_likelihood=jnp.sum(jnp.log(hurdle + gmm)),
+                prev_log_likelihood=log_likelihood,
             )
 
         return lax.while_loop(loop_condition, loop_body, initial_state)
 
     ## Does our data have 3 distinct clusters? --------------------------------
     output = lax.cond(
-        km_out.centroids.shape[0] != 3,
+        jnp.any(X != 0), # & km_out.converged & (centers[0] != centers[1]),
         ## TRUE
-        lambda: while_loop(initial_state),
+        lambda: EM_main_loop(initial_state),
         ## FALSE
         lambda: State(
-            pi=jnp.array([1, 0, 0]),
-            mu=jnp.array([0, 0, 0]),
-            sigma=jnp.array([0, 0]),
+            pi=jnp.array([1., 0., 0.]),
+            mu=jnp.array([jnp.nan, jnp.nan]),
+            sigma=jnp.array([jnp.nan, jnp.nan]),
             r=jnp.zeros((3, n)),
             iterations=0,
-            log_likelihood=0.0,
-            prev_log_likelihood=jnp.inf,
-        )
+            log_likelihood=jnp.nan,
+            prev_log_likelihood=jnp.nan,
+        ),
     )
+    # output = EM_main_loop(initial_state)
 
     return output
 
--- jax_staffr.py
+++ jax_staffr.py
@@ -1,15 +1,19 @@
 import argparse
 import os
+
+os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={os.cpu_count()}"
 import sys
 from functools import partial
-from typing import Any
 
 import jax
 import jax.numpy as jnp
 import pandas as pd
-from tqdm import tqdm
 
 from jax_model_hw import fit_hw_model
+
+
+def unshard(xs):
+    return jax.tree_util.tree_map(lambda x: x.reshape((-1,) + x.shape[2:]), xs)
 
 
 def parse_args():
@@ -41,7 +45,7 @@ def parse_args():
         "--batch-size",
         type=int,
         help="number of lines to process at a time",
-        default=1,
+        default=32,
     )
     parser.add_argument(
         "--data-column-start",
@@ -57,12 +61,7 @@ def parse_args():
         This program will expect one of the fields to be "count" and will use that field as the data.
         """,
     )
-    # parser.add_argument(
-    #     "--acceptance-threshold",
-    #     type=int,
-    #     default=5,
-    #     help="minimum depth of stix evidence to consider as non-zero",
-    # )
+    # TODO reinstate plotting functionality
     # parser.add_argument(
     #     "--plot-distributions",
     #     type=str,
@@ -91,6 +90,7 @@ def main(
     data_field_index = data_column_format.split(":").index("count")
 
     result_keys = ["pi", "mu", "sigma", "log-likelihood", "q"]
+    # TODO reimplement null model in jax
     # if p_value:
     #     result_keys.append("p-value")
 
@@ -101,19 +101,49 @@ def main(
     )
     print(header)
 
-    X = (
+    X = jnp.array(
         data.iloc[:, data_column_start:]
         .map(partial(parse_data_item, data_index=data_field_index))
-        .values
+        .values,
+        dtype=jnp.float32,
+    )
+    print(X.shape)
+
+    # set number of devices (cpu cores) based on batch size
+    fit_func = jax.pmap(
+        fit_hw_model,
+        in_axes=0,
+        # in_axes=(0, None, None, None),
+        static_broadcasted_argnums=(1, 2, 3),
+        devices=jax.devices("cpu")[:batch_size],
     )
 
-    output = jax.vmap(fit_hw_model, in_axes=(0, None, None, None))(
-        X, X.shape[1], convergence, iteration_limit
-    )
+    for batch in range(0, X.shape[0], batch_size):
+        # pad the array to ensure equal batch sizes.  The zip later will skip outputs corresponding to padding.
+        if batch + batch_size > X.shape[0]:
+            x = jnp.pad(X[batch:], ((0, batch + batch_size - X.shape[0]), (0, 0)))
+        else:
+            x = X[batch : batch + batch_size]
 
-    for (_, row), out in zip(data.iterrows(), output):
-        print("\t".join(map(str, row.iloc[:data_column_start])), end="\t")
-        print("\t".join(map(str, [out.pi, out.mu, out.sigma, out.log_likelihood, out.q])))
+        output = fit_func(x, X.shape[1], convergence, iteration_limit)
+        pi, mu, sigma, log_likelihood, Q = (
+            output.pi,
+            output.mu,
+            output.sigma,
+            output.log_likelihood,
+            jnp.sqrt(output.pi[:, 3]),
+        )
+        print(pi.shape, mu.shape, sigma.shape, log_likelihood.shape)
+        for (_, row), p, m, s, l, q in zip(
+            data.iloc[batch : batch + batch_size].iterrows(),
+            pi,
+            mu,
+            sigma,
+            log_likelihood,
+            Q,
+        ):
+            print("\t".join(map(str, row.iloc[:data_column_start])), end="\t")
+            print("\t".join(map(str, [p, m, s, l, q])))
 
 
 if __name__ == "__main__":
